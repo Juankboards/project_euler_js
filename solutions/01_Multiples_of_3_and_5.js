573ec3fc7469f8b47f2191a661ccca2830e16a8a
juankboards <juankboards@gmail.com>
2019-02-18 00:13:27 -0500

    refactor

--- solutions/01_Multiples_of_3_and_5.js
+++ solutions/01_Multiples_of_3_and_5.js
@@ -1,11 +1,9 @@
 //solution-001
 const sumMultiplesOf3And5 = (limit = 0) => { //if not passed, default limit is zero
-	if (limit <= 0) return 0 //we only consider natural numbers
-	let sumMultiplesOf = sumMultiplesBelow(limit)	
+	let sumMultiplesOf = num => num*sequenceSum(parseInt((Math.max(1,limit) - 1)/num))
 	return sumMultiplesOf(3) + sumMultiplesOf(5) - sumMultiplesOf(15)
 }
 
 const sequenceSum = num => (num**2 + num)/2
-const sumMultiplesBelow = limit => num => num*sequenceSum(parseInt((limit - 1)/num))
 
 module.exports = sumMultiplesOf3And5
