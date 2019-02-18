ced931cbae2860504f22a35e0ac5a6d6e17999cf
juankboards <juankboards@gmail.com>
2019-02-18 16:36:32 -0500

    calculate only even fibonacci terms

--- solutions/02_Even_Fibonacci_numbers.js
+++ solutions/02_Even_Fibonacci_numbers.js
@@ -1,10 +1,10 @@
 //solution-002
 const sumEvenTermsOfFibonacciBelow = (limit = 4000000) => { //if not passed, default limit is four millions
-	return sumEvenFibonacciNumbers(limit, sum = 0, firstTerm = 1, secondTerm = 1, nextTerm = firstTerm + secondTerm)
+	return sumEvenFibonacciNumbers(limit, sum = 0, lastEvenTerm = 2, nextEvenTerm = 4*lastEvenTerm)
 }
 
-const sumEvenFibonacciNumbers = (limit, sum, firstTerm, secondTerm, nextTerm) => {
-	return nextTerm > limit? sum : sumEvenFibonacciNumbers(limit, sum += nextTerm%2? 0 : nextTerm, secondTerm, nextTerm, secondTerm + nextTerm)
+const sumEvenFibonacciNumbers = (limit, sum, lastEvenTerm, nextEvenTerm) => {
+	return lastEvenTerm > limit? sum : sumEvenFibonacciNumbers(limit, sum += lastEvenTerm, nextEvenTerm, 4*nextEvenTerm + lastEvenTerm)
 }
 
 module.exports = sumEvenTermsOfFibonacciBelow
